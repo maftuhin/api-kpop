f80086c5100723e965078d202b538ad929e16eda
Maftuhin <maft.maftuhin@gmail.com>
2023-04-24 19:56:00 +0700

    last update limit

--- controller/song.go
+++ controller/song.go
@@ -55,7 +55,7 @@ func LastUpdate(c *fiber.Ctx) error {
 	db := database.DBConn
 
 	var song []models.Song
-	db.Raw("SELECT songs.id, title, uid, language, artists.name as artist FROM songs JOIN artists ON artists.code = songs.artist ORDER BY songs.id DESC LIMIT 5").
+	db.Raw("SELECT songs.id, title, uid, language, artists.name as artist FROM songs JOIN artists ON artists.code = songs.artist WHERE language='original' ORDER BY songs.id DESC LIMIT 7").
 		Scan(&song)
 	return c.JSON(song)
 }
