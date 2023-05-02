0ff770add39c4d14dc0c9bb11a33a3f99434c84e
Maftuhin <maft.maftuhin@gmail.com>
2023-05-02 15:24:29 +0700

    page ost

--- controller/soundtrack.go
+++ controller/soundtrack.go
@@ -4,6 +4,7 @@ import (
 	"maftuhin/kpop-api/database"
 	"maftuhin/kpop-api/models"
 	"maftuhin/kpop-api/tools/paging"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -11,11 +12,13 @@ import (
 func SearchSoundtrack(c *fiber.Ctx) error {
 	db := database.DBConn
 	q := c.Query("q")
+	page, _ := strconv.Atoi(c.Query("page", "1"))
 	var ost []models.Track
 
 	sql := db.Where("title LIKE ?", "%"+q+"%")
 	paging := paging.Paging(&paging.Param{
 		DB:      sql,
+		Page:    page,
 		Limit:   10,
 		OrderBy: []string{"title ASC"},
 	}, ost)
